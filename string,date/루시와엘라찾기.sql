13dfb959f054d96e7118d1794a46b20976412bbc
KilHwanKim <72796198+KilHwanKim@users.noreply.github.com>
2021-01-31 21:18:20 +0900

    Create 루시와엘라찾기.sql

---
+++ string,date/루시와엘라찾기.sql
@@ -0,0 +1,8 @@
+SELECT ANIMAL_ID,NAME,SEX_UPON_INTAKE
+from ANIMAL_INS
+where name  = 'Lucy' or name  ='Ella'
+or name  ='Pickle'
+or name  ='Rogan'
+or name  ='Sabrina'
+or name  ='Mitty'
+order by ANIMAL_ID
