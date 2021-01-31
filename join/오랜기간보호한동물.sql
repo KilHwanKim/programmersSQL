4e50b9e8c319d72f5c6145217da83c3be0ed5565
KilHwanKim <72796198+KilHwanKim@users.noreply.github.com>
2021-01-31 21:16:06 +0900

    Create 오랜기간보호한동물.sql

---
+++ join/오랜기간보호한동물.sql
@@ -0,0 +1,8 @@
+select NAME,DATETIME
+from
+(select I.NAME,I.DATETIME
+from ANIMAL_INS I left join ANIMAL_OUTS O
+on I.ANIMAL_ID = O.ANIMAL_ID
+where O.DATETIME is null
+order by I.DATETIME asc) 
+where rownum < 4
